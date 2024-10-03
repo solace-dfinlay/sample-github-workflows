c51cfceb586171ac2859ad346bc6b4831fd17f60
Neel Patel <>
2024-10-03 14:11:05 -0400

    Change formatting of create_db ai embeddings line

--- tools/create_db.py
+++ tools/create_db.py
@@ -66,7 +66,11 @@ def save_to_chroma(chunks: list[Document]):
 
     # Create a new DB from the documents.
     db = Chroma.from_documents(
-        chunks, OpenAIEmbeddings(base_url="https://lite-llm.mymaas.net/v1", model="bedrock-cohere-embed-english-v3", api_key=os.getenv("LITELLM_API_KEY")), persist_directory=CHROMA_PATH
+        chunks, 
+        OpenAIEmbeddings(base_url="https://lite-llm.mymaas.net/v1", 
+                        model="bedrock-cohere-embed-english-v3",
+                        api_key=os.getenv("LITELLM_API_KEY")),
+        persist_directory=CHROMA_PATH
     )
     db.persist()
     #print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
