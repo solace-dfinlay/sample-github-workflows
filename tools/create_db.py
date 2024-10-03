5a089ef483f52e9a85c91d5171bde767361f2b4c
Neel Patel <>
2024-10-03 14:17:14 -0400

    Increase splitter chunk size

--- tools/create_db.py
+++ tools/create_db.py
@@ -49,8 +49,8 @@ def load_documents():
 
 def split_text(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=100,
+        chunk_size=1000,
+        chunk_overlap=200,
         length_function=len,
         add_start_index=True,
     )
