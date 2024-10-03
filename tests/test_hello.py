95360dd6eb4011817725bd24ba0cff3eeb3202b3
Dylan Finlay <dylan.finlay@solace.com>
2024-10-03 11:52:47 -0400

    fixing tests

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,10 +3,16 @@ from hello import hello, goodbye
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello("Worrld"), "Hello, World!")
+        self.assertEqual(hello("World"), "Hello, World!")
     
     def test_goodbye(self):
-        self.assertEqual(goodbye("Worldd"), "Goodbye, World!")
+        self.assertEqual(goodbye("World"), "Goodbye, World!")
+
+    def test_hello_2(self):
+        self.assertEqual(hello("World"), "Hi, World!")  # This will fail
+    
+    def test_goodbye_2(self):
+        self.assertEqual(goodbye("World"), "Bye, World!")  # This will fail
 
 if __name__ == "__main__":
     unittest.main()
